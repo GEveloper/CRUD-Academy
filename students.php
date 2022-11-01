d05040417b237b7b77bc058acbc96b1cb709b79e
avtojaja <52544731+avtojaja@users.noreply.github.com>
2022-11-01 13:06:46 +0400

    List students as a table.

---
+++ students.php
@@ -0,0 +1,24 @@
+<?php
+include "header.php";
+?>
+    <nav>
+        <a href="students.php" class="active">Students</a>
+        <a href="groups.php">Groups</a>
+        <a href="lecturers.php">Lecturers</a>
+        <a href="teaching.php">Teaching Process</a>
+    </nav>
+
+    <table border="1">
+        <tr>
+            <th>#</th>
+            <th>Student number</th>
+            <th>Student name</th>
+            <th>Gender</th>
+            <th>Group</th>
+            <th>Actions</th>
+        </tr>
+    </table>
+
+<?php
+include "footer.php";
+?>
