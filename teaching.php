a9a910b5775d5746dd69c87eea2d7ab60a2f7de2
s3cb0y <43054353+s3cb0y@users.noreply.github.com>
2022-11-01 14:56:17 +0400

    Include header and footer PHP files.

--- teaching.php
+++ teaching.php
@@ -1,18 +1,25 @@
-<nav>
-    <a href="students.php">Students</a>
-    <a href="groups.php">Groups</a>
-    <a href="lecturers.php">Lecturers</a>
-    <a href="teaching.php" class="active">Teaching Process</a>
-</nav>
+<?php
+include "header.php";
+?>
+    <nav>
+        <a href="students.php">Students</a>
+        <a href="groups.php">Groups</a>
+        <a href="lecturers.php">Lecturers</a>
+        <a href="teaching.php" class="active">Teaching Process</a>
+    </nav>
 
-<a href="teaches-create.php">➕</a>
+    <a href="teaches-create.php">➕</a>
 
-<table border="1">
-    <tr>
-        <th>#</th>
-        <th>Lecturers name</th>
-        <th>Group name</th>
-        <th>Department</th>
-        <th>Actions</th>
-    </tr>
-</table>
+    <table border="1">
+        <tr>
+            <th>#</th>
+            <th>Lecturers name</th>
+            <th>Group name</th>
+            <th>Department</th>
+            <th>Actions</th>
+        </tr>
+    </table>
+
+<?php
+include "footer.php";
+?>
