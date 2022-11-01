86042b037a5d58dd37470e8797b32a2370e32d8f
jaja-x <93482600+jaja-x@users.noreply.github.com>
2022-11-01 15:14:39 +0400

    Add students table creation code.

--- setup.php
+++ setup.php
@@ -22,3 +22,24 @@ if (mysqli_query($link, $sql)) {
 } else {
     echo "Error creating database: " . mysqli_error($link);
 }
+
+mysqli_select_db($link, $database);
+
+// Create table students
+$sql = "CREATE TABLE IF NOT EXISTS `students` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `studnum` int(11) NOT NULL,
+    `fullname` text NOT NULL,
+    `gender` text NOT NULL,
+    `group_id` int(11) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4";
+
+if (mysqli_query($link, $sql)) {
+    echo "students table created successfully<br>";
+} else {
+    echo "Error creating table: " . mysqli_error($link);
+}
+
+// Close connection
+mysqli_close($link);
