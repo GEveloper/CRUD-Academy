beac403e8c4e3aa3b6089b7a99d99038ad74a12b
s3cb0y <43054353+s3cb0y@users.noreply.github.com>
2022-11-01 14:19:59 +0400

    Create groups listing page.

---
+++ groups.php
@@ -0,0 +1,24 @@
+<?php
+include "header.php";
+?>
+    <nav>
+        <a href="students.php">Students</a>
+        <a href="groups.php" class="active">Groups</a>
+        <a href="lecturers.php">Lecturers</a>
+        <a href="teaching.php">Teaching Process</a>
+    </nav>
+
+    <table border="1">
+        <tr>
+            <th>#</th>
+            <th>Group name</th>
+            <th>Department</th>
+            <th>Year</th>
+            <th>Total students</th>
+            <th>Actions</th>
+        </tr>
+    </table>
+
+<?php
+include "footer.php";
+?>
