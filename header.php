803a91593a118aeef4c76e067b37c64f4a9cf4ee
jaja-x <93482600+jaja-x@users.noreply.github.com>
2022-11-01 15:21:35 +0400

    Add navigation CSS styles.

--- header.php
+++ header.php
@@ -8,5 +8,29 @@ include "connection.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>University Application</title>
+    <style>
+        body {
+            margin: 0;
+        }
+        nav {
+            background: gray;
+            text-align: center;
+        }
+        nav a {
+            background: #13d68b;
+            padding: 20px;
+            text-decoration: none;
+            color: white;
+            border-radius: 10px;
+            display: inline-block;
+            margin: 20px;
+        }
+        nav + a {
+            background: #8cd0eb;
+            padding: 20px;
+            display: inline-block;
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
