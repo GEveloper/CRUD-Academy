db5cef07152a23a9dc6797cae30d458511943bed
GEveloper <10330473+GEveloper@users.noreply.github.com>
2022-11-01 12:47:47 +0400

    Add index and other necessary files.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include 'header.php';
+?>
+
+<nav>
+    <a href="students.php">Students</a>
+    <a href="groups.php">Groups</a>
+    <a href="lecturers.php">Lecturers</a>
+    <a href="teaching.php">Teaching Process</a>
+</nav>
+
+<?php
+include 'footer.php';
+?>
