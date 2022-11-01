82c9d0f4ec3e9056d31e8b53aee6820174987750
s3cb0y <43054353+s3cb0y@users.noreply.github.com>
2022-11-01 14:57:04 +0400

    Add SQL code to fetch lecturers and teaching groups.

--- teaching.php
+++ teaching.php
@@ -18,6 +18,31 @@ include "header.php";
             <th>Department</th>
             <th>Actions</th>
         </tr>
+
+        <?php
+            $sql = "
+            SELECT
+                teaches.id,
+                lecturers.fullname,
+                groups.name AS group_name,
+                groups.department
+            FROM teaches
+            JOIN lecturers ON lecturers.id = teaches.lecturer_id
+            JOIN groups ON groups.id = teaches.group_id
+            ORDER BY teaches.id
+            ";
+            $result = mysqli_query($link, $sql);
+
+            while ($teaches = mysqli_fetch_assoc($result)) {
+                echo '<tr id="student-' . $teaches["id"] . '">';
+                echo "<td>" . $teaches["id"] . "</td>";
+                echo "<td>" . $teaches["fullname"] . "</td>";
+                echo "<td>" . $teaches["group_name"] . "</td>";
+                echo "<td>" . $teaches["department"] . "</td>";
+                echo '<td><a href="teaches-delete.php?id=' . $teaches["id"] . '">❌</a></td>';
+                echo "</tr>";
+            }
+        ?>
     </table>
 
 <?php
