beb83bd1ce7765463354d98c33009d8cb051ae98
s3cb0y <43054353+s3cb0y@users.noreply.github.com>
2022-11-01 14:55:41 +0400

    Add teaching page.

---
+++ teaching.php
@@ -0,0 +1,18 @@
+<nav>
+    <a href="students.php">Students</a>
+    <a href="groups.php">Groups</a>
+    <a href="lecturers.php">Lecturers</a>
+    <a href="teaching.php" class="active">Teaching Process</a>
+</nav>
+
+<a href="teaches-create.php">➕</a>
+
+<table border="1">
+    <tr>
+        <th>#</th>
+        <th>Lecturers name</th>
+        <th>Group name</th>
+        <th>Department</th>
+        <th>Actions</th>
+    </tr>
+</table>
