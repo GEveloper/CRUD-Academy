daa735b5d2a726606fee8102a5bca394c5fe3385
avtojaja <52544731+avtojaja@users.noreply.github.com>
2022-11-01 13:07:12 +0400

    Add a loop to iterate each student in a students table.

--- students.php
+++ students.php
@@ -17,6 +17,34 @@ include "header.php";
             <th>Group</th>
             <th>Actions</th>
         </tr>
+
+        <?php
+            $sql = "
+
+            SELECT
+                students.id,
+                students.studnum,
+                students.fullname,
+                students.gender,
+                groups.name AS group_name
+            FROM students
+            LEFT JOIN groups ON groups.id = students.group_id
+            ORDER BY students.id
+
+            ";
+            $result = mysqli_query($link, $sql);
+
+            while ($student = mysqli_fetch_assoc($result)) {
+                echo '<tr id="student-' . $student["id"] . '">';
+                echo "<td>" . $student["id"] . "</td>";
+                echo "<td>" . $student["studnum"] . "</td>";
+                echo "<td>" . $student["fullname"] . "</td>";
+                echo "<td>" . $student["gender"] . "</td>";
+                echo "<td>" . $student["group_name"] . "</td>";
+                echo '<td><a href="student-delete.php?id=' . $student["id"] . '">❌</a></td>';
+                echo "</tr>";
+            }
+        ?>
     </table>
 
 <?php
